c734c21fe7f4eeaab2ec60a45d0a63bded8540ea
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-01-22 20:17:38 -0500

    Project 1

--- Projects/Project 1/main.c
+++ Projects/Project 1/main.c
@@ -3,7 +3,22 @@
 
 int main()
 {
+    int i;
     printf("\n");
-    printf("Enter either 191 or 919, anything else will quit:\n");
+    printf("Enter either 191 or 919, anything else will quit: ");
+    scanf("%d", &i);
+    printf("\n");
+    if(i==191 || i==919){
+        if(i==191){
+        printf("191");
+        }
+        else if(i==919){
+        printf("919");
+    }
+    }// end if
+    else {
+        printf("Goodbye!");
+    }
+
     return 0;
 }
