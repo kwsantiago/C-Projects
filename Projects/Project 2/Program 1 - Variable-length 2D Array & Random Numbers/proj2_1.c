b6c4a85ac4ef122d219fdb3b5987934ad4dbb541
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-01 22:59:39 -0500

    Update proj2_1

--- Projects/Project 2/Program 1 - Variable-length 2D Array & Random Numbers/proj2_1.c
+++ Projects/Project 2/Program 1 - Variable-length 2D Array & Random Numbers/proj2_1.c
@@ -2,27 +2,23 @@
 
 int main(void)
 {
-    int a[n];
+    int n;
+    printf("\nEnter the size of a 2D square array: ");
+    scanf("%d",n);
+    printf("Enter min and max of random numbers to fill in the array.\n");
+    printf("For example, enter -12...8 if min is -12 and max is 8, they must be separated by '...': ");
 
-    printf("Enter the size of a 2D square array: ");
-    scanf("%d", n);
+    printf("\n");
 
-  printf("\nRow totals:");
-  for (i = 0; i < N; i++) {
-    sum = 0;
-    for (j = 0; j < N; j++)
-      sum += a[i][j];
-    printf(" %d", sum);
-  }
+    return 0;
+}// end main
 
-  printf("\nColumn totals:");
-  for (j = 0; j < N; j++) {
-    sum = 0;
-    for (i = 0; i < N; i++)
-      sum += a[i][j];
-    printf(" %d", sum);
-  }
-  printf("\n");
+int array(int n, int m, int a[n][m]){
+    int i, j, sum = 0;
 
-  return 0;
-}
+    for(i = 0; i < n; i++)
+        for(j = 0; j < m; j++)
+            sum += a[i][j];
+
+    return sum;
+}// end function
