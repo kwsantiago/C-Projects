fb5f18cbb20866dddcadfdb8b69bc8ffb63d1c4e
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-01 23:12:21 -0500

    Update 02

--- C Programming A Modern Approach/Projects/Chapter 2/02.c
+++ C Programming A Modern Approach/Projects/Chapter 2/02.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 #define PI 3.14159265358979323846
-#define radius 10
 int main(){
-
-
+    int radius = 10;
+    for(int i = 0; i<2; i++)
+        radius *= radius;
+    float volume = (4.0f/3.0f)*PI*radius;
+    printf("%f", volume);
 }// end main
