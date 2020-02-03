d2b150723bc164b58f82f219d681d1e5af76be95
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-03 00:51:57 -0500

    Fixed wrong order of date

--- C Programming A Modern Approach Projects/Chapter 3/02.c
+++ C Programming A Modern Approach Projects/Chapter 3/02.c
@@ -8,7 +8,7 @@ int main(){
     printf("\nEnter unit price: ");
     scanf("%f",&unit_price);
     printf("\nEnter purchase date (mm/dd/yyy): ");
-    scanf("%d /%d /%d",&day,&month,&year);
+    scanf("%d /%d /%d",&month,&day,&year);
 
     printf("Item\tUnit\tPurchase\n\tPrice\tDate\n%d\t$%.2f\t%d/%d/%d",item_number,unit_price,month,day,year);
 
