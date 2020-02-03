83594ce6b1a2a68041dc03bc5ccd32e1abd5fcd9
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-03 18:17:26 -0500

    Create proj2_3.c

---
+++ Project 2/Program 3 - Recursion/proj2_3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int power(int x, int n);
+
+int main(void){
+
+    int x, n;
+    printf("\nEnter value for x: ");
+    scanf("%d", &x);
+    printf("Enter value for n: ");
+    scanf("%d", &n);
+    printf("%d raised to the power of %d = %d\n", x, n, power(x, n));
+    return 0;
+}// end main
+
+int power(int x, int n){
+    if(n < 0 || x < 0){
+        return 1;
+    }
+
+    if (n == 0){
+    return 1;
+    }
+
+    if (n%2 == 0){
+        int i = power(x, n/2);
+        return i*i;
+    }else{
+        return x * power(x, n-1);
+    }
+}// end function
