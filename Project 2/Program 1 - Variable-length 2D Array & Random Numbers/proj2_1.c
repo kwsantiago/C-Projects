482099d8b10e1e490a8917f7441c4a02c8c288d6
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-04 17:42:48 -0500

    Completed Program 1
    //Minor error with Total rows and columns.

--- Project 2/Program 1 - Variable-length 2D Array & Random Numbers/proj2_1.c
+++ Project 2/Program 1 - Variable-length 2D Array & Random Numbers/proj2_1.c
@@ -1,24 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void)
-{
-    int n;
-    printf("\nEnter the size of a 2D square array: ");
-    scanf("%d",n);
-    printf("Enter min and max of random numbers to fill in the array.\n");
-    printf("For example, enter -12...8 if min is -12 and max is 8, they must be separated by '...': ");
+int RandomNumberGenerator(const int min, const int max,const int d);
 
-    printf("\n");
+int main(void){
+  srand((unsigned) time(NULL));
+  generate_square();
 
-    return 0;
+  return 0;
 }// end main
 
-int array(int n, int m, int a[n][m]){
-    int i, j, sum = 0;
+int generate_square(){
+int d,sum,min,max;
+  int a;
 
-    for(i = 0; i < n; i++)
-        for(j = 0; j < m; j++)
-            sum += a[i][j];
+  printf("\nEnter the size of a 2D square: ");
+  scanf("%d",&d);
+  a = (int*) malloc(d * d * sizeof(int));
 
-    return sum;
+  printf("Enter min and max of random numbers to fill in the array.\n");
+  printf("For example, enter -12...8 if min is -12 and max is 8, they must be separated by '...': ");
+  scanf("%d ... %d",&min,&max);
+
+  int i,j;
+
+  for(i = 0; i<d;i++){
+      for(j = 0; j<d;j++){
+          a = RandomNumberGenerator(min,max,d);
+          printf("%d ",a);
+      }
+      printf("\n");
+  }
+
+  printf("\nRow totals:");
+  for(i = 0; i<d; i++){
+    sum = 0;
+    for(j = 0; j<d; j++)
+      sum += a;
+    printf(" %d", sum);
+  }
+
+  printf("\nColumn totals:");
+  for (j = 0; j < d; j++) {
+    sum = 0;
+    for (i = 0; i < d; i++)
+      sum += a;
+    printf(" %d", sum);
+  }
+
+  printf("\n");
+}
+
+int RandomNumberGenerator(const int min,const int max,const int d){
+  int RandomNumber = 0;
+  for (int i = 0; i < d; i++){
+    RandomNumber = rand()%(max-min) + min;
+    return RandomNumber;
+  }
 }// end function
