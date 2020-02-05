b694b355e47b0acc476603260e0b86e46059068f
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-04 21:52:58 -0500

    Completed proj2_1 \\ All bugs squashed

--- Project 2/Program 1 - Variable-length 2D Array & Random Numbers/proj2_1.c
+++ Project 2/Program 1 - Variable-length 2D Array & Random Numbers/proj2_1.c
@@ -12,39 +12,42 @@ int main(void){
 }// end main
 
 int generate_square(){
-int size,sum,min,max;
-  int a;
+  int size,sum,min,max,i,j;
 
   printf("\nEnter the size of a 2D square: ");
   scanf("%d",&size);
-  a = (int*) malloc(size * size * sizeof(int));
+  int *a[size];
+  for(i = 0; i<size;i++)
+  a[i] = (int*)malloc(size * size * sizeof(int));
 
   printf("Enter min and max of random numbers to fill in the array.\n");
   printf("For example, enter -12...8 if min is -12 and max is 8, they must be separated by '...': ");
   scanf("%d ... %d",&min,&max);
   printf("\n");
 
-  for(int i = 0; i<size;i++){
+
+
+  for(i = 0; i<size;i++){
       for(int j = 0; j<size;j++){
-          a = random_int(min,max);
-          printf("%d ",a);
+          a[i][j] = random_int(min,max);
+          printf("%d ",a[i][j]);
       }
       printf("\n");
   }
 
   printf("\nRow totals:");
-  for(int i = 0; i<size; i++){
+  for (i = 0; i < size; i++) {
     sum = 0;
-    for(int j = 0; j<size; j++)
-      sum += a;
+    for (j = 0; j < size; j++)
+      sum += a[i][j];
     printf(" %d", sum);
   }
 
   printf("\nColumn totals:");
-  for (int j = 0; j < size; j++) {
+  for (j = 0; j < size; j++) {
     sum = 0;
-    for (int i = 0; i < size; i++)
-      sum += a;
+    for (i = 0; i < size; i++)
+      sum += a[i][j];
     printf(" %d", sum);
   }
 
