e3b6f25375bcfc51adaea13440983ce9dbaa54d5
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-08 20:13:52 -0500

    Simplified for loop

--- C Programming A Modern Approach Projects/Chapter 8 - Arrays/07.c
+++ C Programming A Modern Approach Projects/Chapter 8 - Arrays/07.c
@@ -7,8 +7,9 @@ int main(void){
 
     for(i = 0; i < N; i++){
         printf("\nEnter row %d :", i + 1);
-        scanf("%d %d %d %d %d", &a[i][0], &a[i][1],
-                &a[i][2], &a[i][3], &a[i][4]);
+        for(j = 0; j < N; j++){
+            scanf("%d", &a[i][j]);
+            }
         }
 
     printf("\nRow totals: ");
