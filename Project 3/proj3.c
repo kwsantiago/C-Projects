81f65993516fb9552af4ed2cefdb758eaf7299af
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-15 01:19:03 -0500

    Finished function 1

--- Project 3/proj3.c
+++ Project 3/proj3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int menu();
+int random_int(const int min, const int max);
 /* #1 */ void new_accounts(int *, float *, int *, float *);
 /* #2 */ void summary(int, float, int, float);
 /* #3 */ void withdraw(int, float *, float);
@@ -12,19 +13,24 @@ int menu();
 /* #9 */ void change(int *, int);
 
 int main(){
+    srand((unsigned) time(NULL));
     menu();
 
     return 0;
-}
+}// end main
 
 int menu(){
     int n = 1;
+    int account_num1,account_num2;
+    account_num1 = account_num2 = 0;
+    float balance1,balance2;
+    balance1 = balance2 = 0.0f;
     while(n != 0){
         printf("\n\n-------------------------------------------\nMain Menu\n\n1 New Accounts\n2 All Accounts\n3 Withdraw\n4 Deposit\n5 Transfer Fund\n6 Exchange Balance\n7 High Balance\n8 Reset All Balance\n9 Change Account Number\n0 Exit\n-------------------------------------------\n-> Choose your transaction (1 ,2,..., 9, or 0 to exit): ");
         scanf("%d", &n);
         printf("\n>>>> ");
         switch(n){
-            case 1: printf("new account");
+            case 1: new_accounts(&account_num1,&balance1,&account_num2,&balance2);
                 break;
             case 2: printf("all account");
                 break;
@@ -46,4 +52,16 @@ int menu(){
                 break;
         }
     }
-}
+}// end function
+
+void new_accounts(int *account_num1, float *balance1, int *account_num2, float *balance2){
+    *account_num1 = random_int(55,59);
+    *account_num2 = random_int(55,59);
+    *balance1 = (float)((random_int(-10000,99999))/100.0f);
+    *balance2 = (float)((random_int(-10000,99999))/100.0f);
+    printf("Two accounts created!");
+}// end function
+
+int random_int(const int min, const int max){
+   return min + rand() % (max+1 - min);
+}// end function
