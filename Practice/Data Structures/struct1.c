dbac9e9ff69a5a3addcfc8c78d0233a57fd6e2e1
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-02-29 21:58:49 -0500

    Pointer -> Struct

--- Practice/Data Structures/struct1.c
+++ Practice/Data Structures/struct1.c
@@ -7,16 +7,21 @@ typedef struct{
     int seats;
 }CarType;
 
-void printCar(CarType ourCar){
-    printf("\nName: %s\nSpeed: %d\nPeople: %d\nSeats: %d\n", ourCar.name, ourCar.people, ourCar.speed, ourCar.seats);
+void printCar(const CarType *ourCar){
+    printf("\nName: %s\nSpeed: %d\nPeople: %d\nSeats: %d\n", 
+    ourCar->name, 
+    ourCar->people, 
+    ourCar->speed, 
+    ourCar->seats);
 }
 
 int main(){
     CarType car1 = {"Focus", 5, 10, 4};
     CarType car2 = {"Cayman", 3, 55, 6};
 
-    printCar(car1);
-    printCar(car2);
+    printCar(&car1);
+    printCar(&car2);
 
+    printf("\n");
     return 0;
 }
