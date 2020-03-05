90b01586cfef1d362bf9032bb22a50a7fab5527f
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-03-05 18:00:09 -0500

    Create proj4_a.c

---
+++ Project 4/proj4_a.c
@@ -0,0 +1,42 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#define MAX_MSG_LEN 80
+
+void get_msg(char message[], int *length);
+bool palindrome(char message[], int length); 
+
+void get_msg(char message[], int *length){
+    char ch;
+    int count = 0;
+
+    printf("Enter a message: ");
+    while((ch = getchar()) != EOF && ch != '\n'){
+        if (tolower(ch) >= 'a' && toupper(ch) <= 'z'){
+            message[count] = tolower(ch);
+            count++;
+        }
+    }
+    *length = count;
+}
+
+bool palindrome(char message[], int length){
+    int start = 0, end = length - 1;
+
+    while(start < end){
+        if (message[start++] != message[end--])
+            return false;
+    }
+    return true;
+}
+
+int main(void){
+    char message[MAX_MSG_LEN];
+    int *length = (int*)malloc(sizeof(message));
+    get_msg(message, length);
+    printf("%salindrome\n", palindrome(message, *length) ? "P" : "Not a p");
+    free(length);
+    return 0;
+}
