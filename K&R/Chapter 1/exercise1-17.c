ca0476fb1a05e6a2db1f44d6399876c288768457
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-03-09 13:31:20 -0400

    Finalize functions // Completed exercise 17

--- K&R/Chapter 1/exercise1-17.c
+++ K&R/Chapter 1/exercise1-17.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-#define MAXLEN 5000
+#define MAXLEN 1000
+#define LIMIT 80
 
 // Exercise 1-17 of K&R
 // Write a program to print all input lines that are longer than 80 characters.
@@ -8,23 +9,25 @@
 int getLine(char ei[], int lim);
 
 int getLine(char ei[], int lim){
-    int c, i = 0, len = 0;
-
-    while((c = getchar()) != EOF && len < lim){ // While not end of file
-        ++len; // increment len
-        if(c != '\n'){ // while not a space, tab, or new line
-            ei[i] = c;
-            ++i;
-        }
+    int c, i;
+    
+    for(i = 0; i < lim-1 && (c = getchar()) != EOF && c!= '\n'; i++)
+        ei[i] = c; // add each char in current line to current element as long as above is true
+    
+    if(c == '\n'){ // if new line,
+        ei[i] = c; // add to current element and
+        ++i; // go to next element in array
     }
-    ei[i] = '\0';
-    return len;
+    ei[i] = '\0'; // add this to then end to make it a string
+    return i;
 }
 
 int main(){
     char overEighty[MAXLEN];
     int len; 
-    while((len = getLine(overEighty, MAXLEN)) < 80);
-    printf("\n\nLines over 80:\n\n%s", overEighty);
+    while((len = getLine(overEighty, MAXLEN)) > 0){
+        if(len > LIMIT)
+            printf("%s", overEighty);
+    }
     return 0;
 }
