593a9f58c3e3c2cd34ebf8913e6ad134a5832299
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-03-09 23:13:03 -0400

    Completed exercise 20 of Chapter 1

---
+++ K&R/Chapter 1/exercise1-20.c
@@ -0,0 +1,39 @@
+// Exercise 1-20. Write a program detab that replaces tabs in the input 
+// with the proper number
+// of blanks to space to the next tab stop. Assume a fixed set of tab 
+// stops, say every n columns.
+// Should n be a variable or a symbolic parameter? 
+#include <stdio.h>
+
+#define MAXLEN 50
+
+int getLine(int arr[], int lim);
+
+int getLine(int arr[], int lim){
+    int i = 0, c, j;
+    while((c = getchar()) != EOF){
+        if(c == '\t'){ // if tab 
+            for(j = 0; j <= 3; j++){ // add 4 spaces to the array
+                arr[i] = ' ';
+                ++i;
+            }
+        }
+        else{ // else, add input to array
+            arr[i] = c;
+            ++i;
+        }
+    }
+    return i;
+}
+
+int main(){
+    int count, i, arr[MAXLEN];
+
+    for(i = 0; i < MAXLEN; ++i)
+        arr[i] = 0;
+    count = getLine(arr, MAXLEN);
+    for(i = 0; i < count; i++){
+        printf("%c", arr[i]);
+    }
+    return 0;
+}
