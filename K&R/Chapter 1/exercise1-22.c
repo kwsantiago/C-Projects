780aa08c7b3c6b070dd7e39ec8e9456dc97f44bb
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-10 19:16:06 -0400

    Completed Exercise 22 of Chapter 1

--- K&R/Chapter 1/exercise1-22.c
+++ K&R/Chapter 1/exercise1-22.c
@@ -7,26 +7,55 @@
 #include <stdio.h>
 
 #define MAXLEN 1000
+#define TOOBIG 10
 
 int main(){
-    int c, len = 0, i = 0;
-    char arr[MAXLEN];
+    int n, c, len = 0, j = 0, i = 0;
+    char arr[MAXLEN], half[MAXLEN];
     while((c = getchar()) != EOF){
         ++len;
-        if(len > 5){
-            if(c != '\n'){
+        if(c != ' ' && c != '\t' && c != '\n'){
+            if(len > TOOBIG){
+                half[j] = c;
+                ++j;
+            }else if(len <= TOOBIG){
                 arr[i] = c;
                 ++i;
             }
+        }else if(c == ' '){
+            if(len > TOOBIG){
+                half[j] = c;
+                half[++j] = '\n';
+                ++j;
+            }else if(len <= TOOBIG){
+                arr[i] = c;
+                arr[++i] = '\n';
+                ++i;
+            }
         }
-        else if(c != '\n' && len <= 5){
-            arr[i] = c;
-            ++i;
+    }// end while
+    
+    if(i > 0){
+        
+        for(n = 0; n < i/2; n++){
+            printf("%c",arr[n]);
         }
+        printf("\n");
+        for(n = i/2; n < i; n++){
+            printf("%c",arr[n]);
+        }
+        printf("\n");
     }
-
-    for(int j = 0; j < i; j++)
-        printf("%c",arr[j]);
+    if(j > 0){
+        for(n = 0; n < j/2; n++){
+            printf("%c",half[n]);
+        }
+        printf("\n");
+        for(n = j/2; n < j; n++){
+            printf("%c",half[n]);
+        }
+        printf("\n");
+    }
     printf("\n");
     return 0;
 }
