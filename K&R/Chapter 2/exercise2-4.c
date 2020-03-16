37bada376c1917bc5de159cd31e7f58bf391044a
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-15 20:01:13 -0400

    Completed Exercise 4 of Chapter 2

--- K&R/Chapter 2/exercise2-4.c
+++ K&R/Chapter 2/exercise2-4.c
@@ -1,14 +1,23 @@
+// Exercise 2-4. Write an alternative version of squeeze(s1,s2) that deletes each character in s1 that matches any character in the string s2. 
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void getLine(char *p);
-void squeeze(char *p, char *y);
+void squeeze(char *s, const char *reject);
 
-void squeeze(char *p, char *y){
-    for(; *p != '\0'; p++)
-        if(*p != *y++)
-            *p++ = *p;
-    *p++ = '\0';
+void squeeze(char *s, const char *reject){
+    char *j = s;
+    const char *r;
+    for(; *s != '\0'; s++){ // go through main array, one element at a time
+        for(r = reject; *r != '\0'; r++){ // go through array of stuff we don't want
+            if(*s == *r) // if both elements are the same
+                break; // break the loop and go to next element in main array
+        }
+        if(*r == '\0') // if no characters match, add to main array
+            *j++ = *s;
+   }
+   *j = '\0';
 }
 
 void getLine(char *p){
@@ -20,15 +29,12 @@ void getLine(char *p){
     }
     *p++ = '\0';
 }
+
 int main(){
-    char *s, *c, *p, *y;
-    s = malloc(sizeof(char)), c = malloc(sizeof(char));
-    p = s; // p is a pointer to array s
-    y = c; // y is a pointer to array c
-    getLine(p);
-    getLine(y);
-    p = s, y = c; // reset pointer to start of array
-    squeeze(p,y);
+    char *s = malloc(sizeof(char)), *c = malloc(sizeof(char));
+    getLine(s);
+    getLine(c);
+    squeeze(s,c);
     printf("\nSqueeze: %s\n", s);
     return 0;
 }
