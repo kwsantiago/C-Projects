2f69f4db66cdfb7e2adb007e7bf6cfa68b9f40c4
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-03-17 02:09:42 -0400

    Create 01.c

---
+++ C Programming A Modern Approach Projects/Chapter 13 - Strings/01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getInput(char *arr, char *sm, char *la);
+
+int getInput(char *arr, char *sm, char *la){
+    int c, i = 0;
+    while((c = getchar()) != '\n'){
+        i++;
+        *arr = c;
+        arr++;
+    }
+    *arr = '\0';
+    return i;
+}
+
+int main(){
+    char *arr = malloc(sizeof(char)), *smallest = malloc(sizeof(char)), *largest = malloc(sizeof(char));
+    int n;
+    do{
+        printf("Enter word: ");
+    }while((n = getInput(arr, smallest, largest)) != 4);
+    printf("\nSmallest word: %s\nLargest word: %s\n", smallest, largest);
+    free(arr);
+    free(smallest);
+    free(largest);
+    return 0;
+}
