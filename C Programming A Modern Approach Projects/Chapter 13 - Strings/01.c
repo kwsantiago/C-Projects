14ae541d3d39d9c8b326998d1b0ea96958e83500
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-03-17 15:24:22 -0400

    Completed Project 1 of Chapter 13

--- C Programming A Modern Approach Projects/Chapter 13 - Strings/01.c
+++ C Programming A Modern Approach Projects/Chapter 13 - Strings/01.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int getInput(char *arr, char *sm, char *la);
 
 int getInput(char *arr, char *sm, char *la){
-    int c, i = 0;
-    char *tmp = arr;  
-    while((c = getchar()) != '\n'){
-        i++;
-        *arr = c;
-        arr++;
+    while(strlen(arr) != 4){
+        printf("Enter word: ");
+        scanf("%s", arr);
+        if(strcmp(arr, sm) < 0)
+            strcpy(sm, arr);
+        else if(strcmp(arr, la) > 0)
+            strcpy(la, arr);
     }
-    *arr = '\0';
-    for(; *tmp != '\0'; tmp++){
-        *sm = *tmp;
-        *la = *tmp;
-        sm++;
-        la++;
-    }
-    return i;
 }
 
 int main(){
     char *arr = malloc(sizeof(char)), *smallest = malloc(sizeof(char)), *largest = malloc(sizeof(char));
-    int n;
-    do{
-        printf("Enter word: ");
-    }while((n = getInput(arr, smallest, largest)) != 4);
+    printf("Enter word: ");
+    scanf("%s", arr);
+    strcpy(smallest, (strcpy(largest, arr)));
+    getInput(arr, smallest, largest);
     printf("\nSmallest word: %s\nLargest word: %s\n", smallest, largest);
     free(arr);
     free(smallest);
