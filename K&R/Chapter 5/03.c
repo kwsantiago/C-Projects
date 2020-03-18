a724f5bde860d43558f65b5dd4636ec6dc92e7fe
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-17 21:20:59 -0400

    Remove unneeded header

--- K&R/Chapter 5/03.c
+++ K&R/Chapter 5/03.c
@@ -1,7 +1,6 @@
 // Exercise 5-3. Write a pointer version of the function strcat that we showed in Chapter 2: strcat(s,t) copies the string t to the end of s. 
 
 #include <stdio.h>
-#include <stdlib.h>
 
 void strCat(char *s, char *t);
 
