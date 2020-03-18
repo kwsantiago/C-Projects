73c2cb4fe987260449383e289df94d9c6a8c2c7a
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-18 19:47:51 -0400

    Update 10.c

--- K&R/Chapter 5/10.c
+++ K&R/Chapter 5/10.c
@@ -7,12 +7,23 @@
 #include <string.h>
 #include <ctype.h>
 
+#define LEN 10
+
 int rPolish(char *p){
+    int nums[LEN], i = 0, j = 0, answer = 0;
+    char chars[LEN];
+    for(i = 0; i < LEN; i++)
+        nums[i] = 0; // set everything in int array to 0
+    i = 0; // reset i
     for(; *p != '\0'; p++){
-        if(isdigit(*p))
-            i = (*p - '0');
+        if(isdigit(*p)) // check if current element is a digit
+            nums[i++] = (*p - '0'); // set element to numerical value of the char element
+        else
+            chars[j++] = *p; // if not just put it in the char array
     }
-    return i;
+    // math part
+    printf("%c",*p);
+    return answer;
 }
 
 void getInput(char *p){
@@ -26,7 +37,7 @@ void getInput(char *p){
 }
 
 int main(int argc, char *argv[]){
-    char *arr = malloc(sizeof(char)), *p = arr;
+    char *arr = malloc(sizeof(char)), *p = arr, *start = arr;
     getInput(p);
     printf("%d\n", rPolish(p));
     return 0;
