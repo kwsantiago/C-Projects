887562c10bff7e9241b41d763ed6d3b8fff5e767
kyle <44934418+kwsantiago@users.noreply.github.com>
2020-03-20 17:31:33 -0400

    Add comments

--- C Programming A Modern Approach Projects/Chapter 13 - Strings/04.c
+++ C Programming A Modern Approach Projects/Chapter 13 - Strings/04.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-    char **current = &argv[argc-1];
-    char **first = &argv[0];
-    for(; current != first; current--){
-        printf("%s ", *current);
+    char **current = &argv[argc-1]; // last element of input
+    char **first = &argv[0]; // first element of input AKA file name
+    for(; current != first; current--){ // go through the array going backwards
+        printf("%s ", *current); // print the current element
     }
     printf("\n");
     return 0;
