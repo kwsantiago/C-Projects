7335245b1a55ca8706b0e6ac5b5ddf21e3bd6e39
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-03-21 11:27:33 -0400

    Update proj5_1.c

--- Project 5/proj5_1.c
+++ Project 5/proj5_1.c
@@ -8,19 +8,32 @@ float max2(int total, char *data[total]);  // it returns the second maximum of d
 void sum_avg(int total, char *data[total], float *, float *);  // it returns the sum and average of data[1]~data[total] through its 3rd and 4th parameters, respectively.
 
 float min(int total, char *data[total]){
-
+    int i;
+    float min = atof(data[1]); // first input excluding filename
+    for(i = 2; i <= total; i++){ // go through array starting at 2nd input
+        if(atof(data[i]) < min) // if the floating value of teh current element is less than min
+            min = atof(data[i]); // set the value of min to the floating value of current element
+    }
+    return min;
 }
 
-int main(int argc, char* argv[]){
+/*void sum_avg(int total, char *data[total], float *, float *){
     float sum = 0.0;
-    char **p = &argv[1]; // points to first input excluding filename
-    if(argc <= 2){
-        printf("You must enter at least one number after program name.");
-        return 0;
-    }
     for(; *p; p++) // go through the input
         sum += atof(*p); // add the input to sum
     printf("Total: %.3f", sum); // print the total
+}*/
+
+int main(int argc, char* argv[]){
+    if(argc <= 2){
+        printf("You must enter at least one number after program name.\n");
+        return 0;
+    }
+    int total = argc-1;
+    float *sum, *average;
+    printf("Total numbers entered = %d\n", total);
+    printf("min = %.3f\n", min(total, argv));
+    printf("min2 = %.3f\n", min(total, argv));
     printf("\n");
     return 0;
 }
