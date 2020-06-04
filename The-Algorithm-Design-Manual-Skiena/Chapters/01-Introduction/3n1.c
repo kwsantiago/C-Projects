121958074b01d12b559848b7af3b7c54dba59456
kwsantiago <kwsantiago@mail.usf.edu>
2020-06-04 01:15:31 -0400

    Add functions

--- The-Algorithm-Design-Manual-Skiena/Chapters/01-Introduction/3n1.c
+++ The-Algorithm-Design-Manual-Skiena/Chapters/01-Introduction/3n1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int genNumbers(int num);
+int genNumbersMax(int a, int b);
 
 int genNumbers(int num){
     printf("%d ", num);
@@ -12,7 +13,26 @@ int genNumbers(int num){
         genNumbers((num*3)+1);
 }
 
-int main(int argc, char* argv[]){
-    genNumbers(22);
+int genNumbersMax(int a, int b){
+    if(a > b){
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    int maxNum = 0;
+
+    for(; a <= b; a++){
+        int thisNumSize = genNumbers(a);
+        if(thisNumSize > maxNum)
+            maxNum = thisNumSize;
+    }
+    return maxNum;
+}
+
+int main() {
+    int a, b;
+    while (scanf("%d %d", &a, &b) != EOF) {
+        printf("%d %d %d\n", a, b, genNumbersMax(a, b));
+    }
     return 0;
 }
